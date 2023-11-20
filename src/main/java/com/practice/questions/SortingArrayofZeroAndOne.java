4ec4c67a0ea523d3108d9fa10bab2490ce018ea6
Abhijeet Sakhalkar <abhijeet.sakhalkar@bp.com>
2023-11-20 16:52:20 +0530

    Sorting array of 0 and 1

--- src/main/java/com/practice/questions/SortingArrayofZeroAndOne.java
+++ src/main/java/com/practice/questions/SortingArrayofZeroAndOne.java
@@ -2,7 +2,7 @@ package com.practice.questions;
 
 import java.util.Arrays;
 
-public class MyTest {
+public class SortingArrayofZeroAndOne {
     public static void main(String[] args) {
         int [] arr = new int []{0,1,0,1,1,1, 0,0,1,1,0,0,0};
         int left = 0;
